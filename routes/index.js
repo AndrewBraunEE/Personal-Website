f6c6c5f201c9ca436b8e6991a0f8b1dfd37fa5bc
Andrew Braun <9020619+AndrewBraunEE@users.noreply.github.com>
2017-09-19 11:44:51 -0700

    NodeJS Update

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/index.html', function(req,res,next){
+	res.sendFile('index.html', {root : __dirname});
+});
+
+router.get('/links.html', function(req,res,next){
+	res.sendFile('links.html', {root : __dirname});
+});
+
+router.get('/resume.docx', function(req,res,next){
+	res.sendFile('resume.docx', {root : __dirname});
+});
+
 router.get('/viewstream.html', function(req,res,next){ //From here, route a streambuffer from a PC, send it to the client, and have it played as a stream.
 });
 
