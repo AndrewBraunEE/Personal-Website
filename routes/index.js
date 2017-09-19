b3ee1e0f9404d69ce51617ccbdfd7f67612386de
Andrew Braun <9020619+AndrewBraunEE@users.noreply.github.com>
2017-09-19 00:05:19 -0700

    NodeJS Config

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-function getURL(localurl){ 
-	router.get(localurl, function(req,res,next)){
-		res.sendfile(localurl);
-	};
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/viewstream.html', function(req,res,next)){ //From here, route a streambuffer from a PC, send it to the client, and have it played as a stream.
-}
+router.get('/viewstream.html', function(req,res,next){ //From here, route a streambuffer from a PC, send it to the client, and have it played as a stream.
+});
 
 module.exports = router;
